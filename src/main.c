10f10619213cc7a914587106bdd08af87e6b7161
Jouri Abrosimov <abrossimov@mail.ru>
2021-02-01 21:01:19 +0300

    lesson 28

--- src/main.c
+++ src/main.c
@@ -1,27 +1,9 @@
 #include "main.h"
 #include "ariph.h"
 #include "utils.h"
-//----------------------------------------------
-/*
-struct student
-{
-  char nm[60];
-  unsigned char age;
-  unsigned char course;
-};
-*/
-typedef struct
-{
-  char nm[60];
-  unsigned char age;
-  unsigned char course;
-} student;
+#include "student.h"
 //----------------------------------------------
 unsigned char student_counter=0;
-//----------------------------------------------
-student addStudent (char ch[], unsigned int cr, unsigned int ag);
-//----------------------------------------------
-typedef unsigned int my_int32;
 //----------------------------------------------
 float res;
 char str1[30] = {};
@@ -29,127 +11,52 @@ float yf, zf;
 //----------------------------------------------
 int main()
 {
-  unsigned int a[10] = {0x33333333, 0x44444444, 0x55555555, 0x66666666, 0x77777777, \
-                        0x88888888, 0x99999999, 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC};
+/*
+  struct student st1;
+  strcpy(st1.nm, "Иванов Иван Иванович");
+  st1.age = 18;
+  st1.course = 2;
 
-  printf("Element addresses a\n");
-    for(int i=0; i<10;i++)
-    {
-      printf("%08X ",(unsigned int) &a[i]);
-    }
-    printf("\nValue a\n");
-    for(int i=0; i<10;i++)
-    {
-      printf("%08X ",a[i]);
-    }
-  printf("\n--------------------------------------------\n");
-    unsigned int *p0_a, *p5_a;
-    p0_a = &a[0];
-    p5_a = &a[5];
-    printf("Value *p0_a is %08X\tp0_a is %08X\n", *p0_a, (unsigned int) p0_a);
-    printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  printf("\n--------------------------------------------\n");
-    p0_a++; p5_a++;
-    printf("p0_a++; p5_a++;\n");
-    printf("Value *p0_a is %08X\tp0_a is %08X\n", *p0_a, (unsigned int) p0_a);
-    printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  printf("\n--------------------------------------------\n");
-    p0_a--; p5_a--;
-    printf("p0_a--; p5_a--;\n");
-    printf("Value *p0_a is %08X\tp0_a is %08X\n", *p0_a, (unsigned int) p0_a);
-    printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  printf("\n--------------------------------------------\n");
-    p0_a+=4; p5_a+=2;
-     printf("p0_a+=4; p5_a+=2;\n");
-     printf("Value *p0_a is %08X\tp0_a is %08X\n", *p0_a, (unsigned int) p0_a);
-     printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  printf("\n--------------------------------------------\n");
-    p0_a-=4; p5_a-=2;
-    printf("p0_a-=4; p5_a-=2;\n");
-    printf("Value *p0_a is %08X\tp0_a is %08X\n", *p0_a, (unsigned int) p0_a);
-    printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  printf("\n--------------------------------------------\n");
-  unsigned int n=p5_a-p0_a;
-  printf("Value n is %u\n",n);
-  printf("\n--------------------------------------------\n");
-  /*
-  n=*++p5_a;// сначала инкремент адреса потом его забираем и присваиваем в n
-  printf("n is %08X\n", n);
-  printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  */
-  /*
-  n=++*p5_a;// забираем значение, а потом его увеличиваем +1 и присваиваем n
-  printf("n is %08X\n", n);
-  printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  */
-  /*
-  n=*p5_a++;// сначала забрать значение и присваиваем в n потом увеличить адрес
-  printf("n is %08X\n", n);
-  printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  */
-  /*
-  n=(*p5_a)++;// сначала забрали значние по адресу  и присвоили в n , потом увеличили начение по адрессу указателя
-  printf("n is %08X\n", n);
-  printf("Value *p5_a is %08X\tp5_a is %08X\n", *p5_a, (unsigned int) p5_a);
-  */
-  unsigned int b[4] = {0x00112233, 0x44556677, 0x8899AABB, 0xCCDDEEFF};
-    printf("Element addresses b\n");
-    for(int i=0; i<4;i++)
-    {
-      printf("%08X ",(unsigned int) &b[i]);
-    }
-    printf("\nValue b\n");
-    for(int i=0; i<4;i++)
-    {
-      printf("%08X ",b[i]);
-    }
-  printf("\n--------------------------------------------\n");
-    unsigned int *p0_b = &b[0], *p2_b = &b[2];
-    printf("Value *p0_b is %08X\tp0_a is %08X\n", *p0_b, (unsigned int) p0_b);
-    printf("Value *p2_b is %08X\tp5_a is %08X\n", *p2_b, (unsigned int) p2_b);
-  printf("\n--------------------------------------------\n");
-    printf("Value p0_b[0] is %08X\n", p0_b[0]);
-    printf("Value p0_b[1] is %08X\n", p0_b[1]);
-    printf("Value p2_b[0] is %08X\n", p2_b[0]);
-    printf("Value p2_b[1] is %08X\n", p2_b[1]);
-    printf("Value p2_b[2] is %08X\n", p2_b[2]);// вышли за границу
-
-    printf("Value p2_b[-1] is %08X\n", p2_b[-1]);
-    printf("Value p0_b[-1] is %08X\n", p0_b[-1]); //вышли за границу
-  printf("\n--------------------------------------------\n");
-    printf("Value *b is %08X\n", *b);
-    printf("Value *(b+2) is %08X\n", *(b+2));
-    printf("Value (unsigned int)(unsigned char*)b is %08X\n", (unsigned int)(unsigned char*)b);
-    printf("Value *(unsigned char*)b is %02X\n", *(unsigned char*)b);
-    printf("Value *(unsigned char*)b is %02X\n", *(unsigned char*)(b+1));
-    printf("Value *(unsigned char*)b is %02X\n", *(unsigned char*)(b+2));
-
-    printf("Value *(((unsigned char*)b)+1) is %02X\n", *(((unsigned char*)b)+1));
-    printf("Value *(((unsigned char*)b)+2) is %02X\n", *(((unsigned char*)b)+2));
-    printf("Value *(((unsigned char*)b)+3) is %02X\n", *(((unsigned char*)b)+3));
+  struct student *st1_p = &st1;
+  printf("%s,\t%d курс,\t%d лет\n", (*st1_p).nm, (*st1_p).course, (*st1_p).age);
 
   printf("\n--------------------------------------------\n");
-    printf("bytes of b\n");
-    printf("0x%08X\n", (unsigned int)&b[0]);
-    for(int i=0; i<16; i++)
-    {
-      printf("%02X ", (unsigned int)(((unsigned char*)b)+i)&0x000000FF);
-    }
-    printf("\n");
-    for(int i=0; i<16; i++)
-    {
-      printf("%02X ", *(((unsigned char*)b)+i));
-    }
+
+  struct student st2;
+  strcpy(st2.nm, "Перов Петр Иванович");
+  st2.age = 17;
+  st2.course = 1;
+
+  struct student *st2_p = &st2;
+  printf("%s,\t%d курс,\t%d лет\n", st2_p->nm, st2_p->course, st2_p->age);
+
+  printf("\n--------------------------------------------\n");
+
+  struct student st3;
+  struct student *st3_p = &st3;
+  strcpy(st3_p->nm, "Семенов Игорь Васильевич");
+  st3_p->age = 20;
+  st3_p->course = 3;
+
+  printf("%s,\t%d курс,\t%d лет\n", st3_p->nm, st3_p->course, st3_p->age);
+*/
+ /*
+struct student
+ {
+   char nm[60];
+   unsigned char age;
+   unsigned char course;
+ } st;
+*/
+
+  student st;
+  student *st_p=&st;
+  strcpy(st_p->nm, "Иванов Иван Иванович");
+  st_p->age = 18;
+  st_p->course = 2;
+  printf("%s,\t%d курс,\t%d лет\n", st_p->nm, st_p->course, st_p->age);
+
   return 0; //Return an integer from a function
 }
 //--------------------------------------------------------
-student addStudent (char ch[], unsigned int cr, unsigned int ag)
-{
-  student res_st;
-  strcpy(res_st.nm, ch);
-  res_st.course = cr;
-  res_st.age = ag;
-  student_counter++;
-  return res_st;
-}
-//--------------------------------------------------------
+
